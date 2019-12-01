a80fe4b114c41d5dd329da5264dbe94df46a2c57
ndhungw <56079549+ndhungw@users.noreply.github.com>
2019-12-01 22:28:22 +0700

    - ở phần insert data, bỏ id của bảng NhanVien, HoaDon, MonAn - thử đổ dữ liệu món ăn vào Admin->Thức ăn

--- fAdmin.cs
+++ fAdmin.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using wf_CanteenManagement.DAO;
 
 namespace wf_CanteenManagement
 {
@@ -15,6 +17,16 @@ namespace wf_CanteenManagement
         public fAdmin()
         {
             InitializeComponent();
+
+            loadFoodList();
+        }
+
+        void loadFoodList()
+        {
+            string query = "SELECT * FROM dbo.MonAn";
+            DataProvider provider = new DataProvider();
+
+            dtgvFood.DataSource = provider.ExecQuery(query);
         }
     }
 }
